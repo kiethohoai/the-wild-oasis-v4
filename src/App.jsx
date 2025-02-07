6b76b4cfc551e2afcd61c1237c2579256c5249ce
David Ho <kietho.060193>
2025-02-07 10:43:10 +0700

    setup app layout

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import {
   Users,
 } from './pages';
 import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './ui/AppLayout';
 
 const App = () => {
   return (
@@ -20,13 +21,18 @@ const App = () => {
       {/* App */}
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          {/* App Layout */}
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
+          {/* Others */}
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
