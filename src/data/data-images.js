508abbf93f37bdac3bc99ad27c6999f1e416dd3b
David Ho <kietho.060193>
2025-02-08 14:43:53 +0700

    add form errors

---
+++ src/data/data-images.js
@@ -0,0 +1,10 @@
+export const defaultImages = [
+  'https://lgqroqhfwyzolpyyvjhp.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg',
+  'https://lgqroqhfwyzolpyyvjhp.supabase.co/storage/v1/object/public/cabin-images//cabin-002.jpg',
+  'https://lgqroqhfwyzolpyyvjhp.supabase.co/storage/v1/object/public/cabin-images//cabin-003.jpg',
+  'https://lgqroqhfwyzolpyyvjhp.supabase.co/storage/v1/object/public/cabin-images//cabin-004.jpg',
+  'https://lgqroqhfwyzolpyyvjhp.supabase.co/storage/v1/object/public/cabin-images//cabin-005.jpg',
+  'https://lgqroqhfwyzolpyyvjhp.supabase.co/storage/v1/object/public/cabin-images//cabin-006.jpg',
+  'https://lgqroqhfwyzolpyyvjhp.supabase.co/storage/v1/object/public/cabin-images//cabin-007.jpg',
+  'https://lgqroqhfwyzolpyyvjhp.supabase.co/storage/v1/object/public/cabin-images//cabin-008.jpg',
+];
