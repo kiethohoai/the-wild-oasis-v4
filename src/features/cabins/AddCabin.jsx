6b5f2a8a0822ea7988de39a415945a638786fd7f
David Ho <kietho.060193>
2025-02-09 17:52:01 +0700

    convert the modal

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,8 +1,35 @@
-import { useState } from 'react';
 import Button from '../../ui/Button';
 import Modal from '../../ui/Modal';
+import CabinTable from './CabinTable';
 import CreateCabinForm from './CreateCabinForm';
 
+const AddCabin = () => {
+  return (
+    <Modal>
+      {/* #1 */}
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      {/* #2 */}
+      <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
+  );
+};
+
+export default AddCabin;
+
+/* 
 const AddCabin = () => {
   const [isOpenModal, setIsOpenModel] = useState(false);
 
@@ -21,4 +48,4 @@ const AddCabin = () => {
   );
 };
 
-export default AddCabin;
+export default AddCabin; */
