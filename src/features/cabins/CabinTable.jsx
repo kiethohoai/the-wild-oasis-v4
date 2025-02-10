296a294d8c94c364d506394e9ff443db9c030906
David Ho <kietho.060193>
2025-02-10 17:50:40 +0700

    filter cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,15 +3,26 @@ import CabinRow from './CabinRow';
 import useCabins from './useCabins';
 import Table from '../../ui/Table';
 import Menus from '../../ui/Menus';
+import { useSearchParams } from 'react-router-dom';
 
 const CabinTable = () => {
   // get cabins data
   const { cabins, isLoading } = useCabins();
+  const [searchParams] = useSearchParams();
 
   // while loading...
   if (isLoading) {
     return <Spinner />;
   }
+
+  const filterValue = searchParams.get('discount') || 'all';
+
+  let filterCabins;
+  if (filterValue === 'all') filterCabins = cabins;
+  if (filterValue === 'no-discount')
+    filterCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === 'with-discount')
+    filterCabins = cabins.filter((cabin) => cabin.discount > 0);
 
   return (
     <Menus>
@@ -26,7 +37,8 @@ const CabinTable = () => {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          // data={cabins}
+          data={filterCabins}
           render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
         />
       </Table>
