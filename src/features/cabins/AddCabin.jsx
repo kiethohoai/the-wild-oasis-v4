d9787e367ba1ee78518d81333fe2ab0797300aac
David Ho <kietho.060193>
2025-02-10 15:58:03 +0700

    reused table

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -19,33 +19,3 @@ const AddCabin = () => {
 };
 
 export default AddCabin;
-
-/* 
-      <Modal.Open opens="table">
-        <Button>Show table</Button>
-      </Modal.Open>
-
-      <Modal.Window name="table">
-        <CabinTable />
-      </Modal.Window>
-
-
-const AddCabin = () => {
-  const [isOpenModal, setIsOpenModel] = useState(false);
-
-  return (
-    <div>
-      <Button onClick={() => setIsOpenModel(!isOpenModal)}>
-        Add New Cabin
-      </Button>
-
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModel(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModel(false)} />
-        </Modal>
-      )}
-    </div>
-  );
-};
-
-export default AddCabin; */
